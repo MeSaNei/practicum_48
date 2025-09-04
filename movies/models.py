942754a15ae49d024508ebd4fc0cfd6fccc5f849
Vitalii Panikhin <vpanihin@yandex.ru>
2025-09-05 00:44:14 +0300

    - описал модель content.genre - включил в админку приложения movies - описал и подключил модель film_works Кинопроизведения

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,45 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    # Типичная модель в Django использует число в качестве id. В таких ситуациях поле не описывается в модели.
+    # Вам же придётся явно объявить primary key.
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Первым аргументом обычно идёт человекочитаемое название поля
+    name = models.CharField('name', max_length=255)
+    # blank=True делает поле необязательным для заполнения.
+    description = models.TextField('description', blank=True)
+    # auto_now_add автоматически выставит дату создания записи 
+    created = models.DateTimeField(auto_now_add=True)
+    # auto_now изменятся при каждом обновлении записи 
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
+        db_table = "content\".\"genre"
+        # Следующие два поля отвечают за название модели в интерфейсе
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+class FilmWork(models.Model):
+    class Type(models.TextChoices):
+        MOVIE = 'movie', 'Movie'
+        TV_SHOW = 'tv_show', 'TV Show'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField('title', max_length=255)
+    description = models.TextField('description', blank=True)
+    creation_date = models.DateField('creation date', blank=True, null=True)
+    rating = models.FloatField('rating', blank=True, null=True)
+    type = models.CharField('type', max_length=50, choices=Type.choices)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "content\".\"film_work" 
+        verbose_name = 'Кинопроизведение'
+        verbose_name_plural = 'Кинопроизведения'
+
+    def __str__(self):
+        return self.title
